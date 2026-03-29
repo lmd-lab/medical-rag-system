87e368898fc20b468fddb81a7c0e08e470ed48b5
lmd-lab <lena.dietsche@gmail.com>
2026-03-29 20:48:05 +0200

    feat: add constants for raw and processed data paths in config file

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+RAW_DATA_PATH = Path("data/raw")
+PROCESSED_DATA_PATH = Path("data/processed")
+
+#RAW_DATA_PATH_TEST = Path("notebooks/raw_test")
+#PROCESSED_DATA_PATH_TEST = Path("notebooks/processed_test")
